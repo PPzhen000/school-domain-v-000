1539b4ea920b649ce0ecfb67c9a75ce3883a2b5c
Ariel Zhen <zhenpingping@live.com>
2018-05-31 22:17:36 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School 
+
+  def initialize(name)
+    @name = name 
+    @rooster = {} 
+  end 
+
+  def name
+    @name 
+  end 
+
+  def add_student(name, grade)
+    if @rooster["#{grade}"] == undefined
+    @rooster["#{grade}"] = []
+    end 
+    @rooster["#{grade}"] << name 
+  end 
+
+  def grade(grade)
+    @rooster["#{grade}"] 
+  end 
+end 
