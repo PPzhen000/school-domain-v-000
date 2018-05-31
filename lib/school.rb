9ad38878d8c112061275d21b09f147b2f1939b0c
Ariel Zhen <zhenpingping@live.com>
2018-05-31 22:25:03 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -16,10 +16,10 @@ class School
   end
 
   def add_student(name, grade)
-    if @roster["#{grade}"] == nil 
-    @roster["#{grade}"] = []
+    if @roster[grade] == nil 
+    @roster[grade] = []
     end
-    @roster["#{grade}"] << name
+    @roster[grade] << name
   end
 
   def grade(grade)
